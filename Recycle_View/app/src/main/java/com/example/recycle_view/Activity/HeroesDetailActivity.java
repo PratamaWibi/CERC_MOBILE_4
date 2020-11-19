af197e06b8ff27836bb5619a1dff1fdb48ad48b5
Pratama Wibi <uchihapratama135@gmail.com>
2020-11-19 19:39:13 +0700

    Recycle View

---
+++ Recycle_View/app/src/main/java/com/example/recycle_view/Activity/HeroesDetailActivity.java
@@ -0,0 +1,42 @@
+package com.example.recycle_view.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.cerc_materialdesign.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class HeroesDetailActivity extends AppCompatActivity {
+
+    private CircleImageView circleImageView;
+    private TextView tvNama;
+    private TextView tvFrom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heroes_detail);
+
+        circleImageView = findViewById(R.id.img_item_photo);
+        tvNama = findViewById(R.id.tvNama);
+        tvFrom = findViewById(R.id.tvAsal);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String from = intent.getStringExtra("from");
+        String photo = intent.getStringExtra("photo");
+        Glide.with(getApplicationContext())
+                .load(photo)
+                .apply(new RequestOptions().override(350, 550))
+                .into(circleImageView);
+        tvNama.setText(name);
+        tvFrom.setText(from);
+
+    }
+}
